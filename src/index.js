6e41b19abee156ff92148def9d7ad718cad0a8bf
Johann Prell <johann.prell@gmail.com>
2019-11-02 14:37:35 +0100

    eslint init - prepare db - stared util with math.randomInt

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
+import * as UTIL from './util'
+
 window.onload = init
+
+let mainText
 
 function init() {
   console.log('This is a Random Venture.')
 
+  initMainText()
   initButtons()
+
+  print('Welcome to Random Venture')
+}
+
+function initMainText() {
+  mainText = document.getElementById('main-text')
 }
 
 function initButtons() {
@@ -14,4 +25,13 @@ function initButtons() {
 
 function onChoice() {
   console.log('Wise choice')
+  print(getRandomText())
+}
+
+function print(text) {
+  mainText.innerHTML = text
+}
+
+function getRandomText() {
+  return 'this is a random text ' + UTIL.math.randomInt(0, 100)
 }
