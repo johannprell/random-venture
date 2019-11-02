3b643aeeb964471e2ee2e83bb0c3de7ecbb683fe
Johann Prell <johann.prell@gmail.com>
2019-11-02 14:04:03 +0100

    basic structure - buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-init()
+window.onload = init
 
 function init() {
   console.log('This is a Random Venture.')
-  
+
+  initButtons()
+}
+
+function initButtons() {
+  document.getElementById('btn-choice-01').onclick = onChoice
+  document.getElementById('btn-choice-02').onclick = onChoice
+
+}
+
+function onChoice() {
+  console.log('Wise choice')
 }
