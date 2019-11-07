6b3490ef94337437fc6def793f9e6d396b275352
Johann Prell <johann.prell@gmail.com>
2019-11-07 22:23:11 +0100

    simplify
    since state moves forward one template at the time, templates should have information embedded on where to move next

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
 import * as UI from './ui'
 import * as TMPL from './template'
 
+let currentTemplate
 window.onload = init
 
 function init() {
   console.log('This is a Random Venture.')
   UI.init()
-  UI.apply(TMPL.introduction())
-  // Never ending placeholder gameloop
+  currentTemplate = TMPL.introduction
+  UI.apply(currentTemplate)
+  // Game loop only ticks on choice button input
   UI.setOnChoiceCallback(
-    placeholderCallback
+    evaluateChoice
   )
 }
 
-function placeholderCallback(id) {
+function evaluateChoice(id) {
   console.log(id)
   UI.apply(TMPL.example())
 }
